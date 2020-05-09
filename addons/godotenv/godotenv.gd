8a8457204cd86ef31d0e043003776a973534ea84
Lucas Coelho <lucascoelhodacosta@gmail.com>
2020-05-09 17:45:54 -0300

    Add basic code documentation

--- addons/godotenv/godotenv.gd
+++ addons/godotenv/godotenv.gd
@@ -1,21 +1,27 @@
 tool
 extends Node
 
+# Constants for .env file location
 const FILE_NAME: String = ".env"
 const FILE_PATH: String = "res://%s" % FILE_NAME
+
+# Prefix for error and warning messages, make easier to identify the source
 const ERROR_PREFIX: String = "GodotEnv: "
 
+# Possible warnings and errors to make it easier to re-use and add if needed
 var prompt: Dictionary = {
 	"unexisting_file_error": ".env file doesn't exists at %s." % FILE_PATH,
 	"creating_dotenv_file": ".env file doesn't exists. Creating one at %s..." % FILE_PATH,
 	"already_exists": "A variable called %s already exists, use argument override=true to override it."
 }
 
+# Every KEY=VALUE pair from .env will be parsed into this dictionary
 var environment: Dictionary = {}
 
 func _ready() -> void:
 	environment = get_parsed_env()
 
+# Custom function to prompt error and warning messages
 func prompt_message(msg: String, error: bool = false) -> void:
 	if error:
 		push_error(ERROR_PREFIX + msg)
@@ -49,25 +55,29 @@ func overwrite_dotenv() -> void:
 	dot_env.open(FILE_PATH, File.WRITE)
 	dot_env.store_string(new_content)
 
-func create(variable_name: String, value: String, override: bool = false) -> void:
+func create(variable_name: String, value: String, overwrite: bool = false) -> void:
 	var dot_env: File = File.new()
 	if not dot_env.file_exists(FILE_PATH):
+#		Create a .env file to store the new variable if file doesn't already exists
 		prompt_message(prompt.creating_dotenv_file)
 		dot_env.open(FILE_PATH, File.WRITE)
 		dot_env.store_string("%s=%s" % [variable_name, value])
 		dot_env.close()
+		return
 		
-	
-	if environment.has(variable_name) and not override:
+	if environment.has(variable_name) and not overwrite:
+#		Prevent user from accidentally overwriting an env var
 		prompt_message(prompt.already_exists % variable_name)
 		return
-	elif environment.has(variable_name) and override:
+	elif environment.has(variable_name) and overwrite:
+#		Overwrite an already existing env variable
 		environment[variable_name] = value
 		overwrite_dotenv()
 		return
 	
 	dot_env.open(FILE_PATH, File.READ_WRITE)
 	dot_env.seek_end()
+#	Append the new env var to the end of an existing .env file
 	dot_env.store_string("\n%s=%s" % [variable_name, value])
 	dot_env.close()
 	environment = get_parsed_env()
--- addons/godotenv/init.gd
+++ addons/godotenv/init.gd
@@ -5,8 +5,10 @@ const AUTOLOAD_NAME: String = "GodotEnv"
 const AUTOLOAD_PATH: String = "res://addons/godotenv/godotenv.gd"
 
 func _enter_tree():
+#	Used to automatically add GodotEnv as a autoload script when activated
 	add_autoload_singleton(AUTOLOAD_NAME, AUTOLOAD_PATH)
 
 func _exit_tree():
+#	Remove when it's necessary anymore
 	remove_autoload_singleton(AUTOLOAD_NAME)
 
